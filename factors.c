1df5f42d7e5b354db46af016dfa4fbd098a3df38
Vincidax <vincentdx18@gmail.com>
2023-09-02 13:08:38 -0700

    invalid

--- factors.c
+++ factors.c
@@ -1,55 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * factorize - Factorizes an integer and prints its prime factors.
+ * factorize - Factorizes an integer and prints the result.
  * @number: The integer to factorize.
  */
 void factorize(int number)
 {
-    int p, q;
+	int p, q;
 
-    for (p = 2; p <= number; ++p)
-    {
-        if (number % p == 0)
-        {
-            q = number / p;
-            printf("%d = %d * %d\n", number, p, q);
-            return;
-        }
-    }
-    printf("Unable to factorize %d\n", number);
+	for (p = 2; p <= number; ++p)
+	{
+		if (number % p == 0)
+		{
+			q = number / p;
+			printf("%d = %d * %d\n", number, p, q);
+			return;
+		}
+	}
 }
 
+/**
+ * main - Reads integers from a file and attempts to factorize them.
+ * @argc: The number of command-line arguments.
+ * @argv: The array of command-line arguments.
+ * Return: 0 on success, non-zero on failure.
+ */
 int main(int argc, char *argv[])
 {
-    char *file_path;
-    FILE *file;
-    char line[256]; /* Assuming lines in the file are not longer than 255 characters */
+	char *file_path;
+	FILE *file;
+	char line[256]; /* Assuming lines in the file are not longer than 255 chars */
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return (1);
+	}
+	file_path = argv[1];
+	file = fopen(file_path, "r");
 
-    file_path = argv[1];
-    file = fopen(file_path, "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "File '%s' not found.\n", file_path);
+		return (1);
+	}
 
-    if (file == NULL)
-    {
-        fprintf(stderr, "File '%s' not found.\n", file_path);
-        return 1;
-    }
+	while (fgets(line, sizeof(line), file))
+	{
+		int num = atoi(line);
 
-    while (fgets(line, sizeof(line), file))
-    {
-        int num = atoi(line);
-
-        factorize(num);
-    }
-
-    fclose(file);
-    return 0;
+		if (num <= 1)
+		{
+			fprintf(stderr, "Invalid number in the file: %d\n", num);
+			continue;
+		}
+		factorize(num);
+	}
+	fclose(file);
+	return (0);
 }
